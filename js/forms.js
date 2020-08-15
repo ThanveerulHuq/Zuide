420a382976deb22f4a2470ae98134611ddec16f0
thanveer <thanveerul.haq@nielsen.com>
2020-08-15 16:46:29 +0530

    form submission added

--- js/forms.js
+++ js/forms.js
@@ -47,64 +47,64 @@ function resetFormError(message, interval){
 var isSend = true;
 var newQuery = true;
 
-$('.notify-me').submit(function(e){
-	var form           = $(this),
-		message        = form.find('.form-message'),
-		messageSuccess = 'Your email has been sent',
-		messageInvalid = 'Please enter a valid email address',
-		messageSigned  = 'This email is already signed',
-		messageErrore  = 'Error request';
-	e.preventDefault();
-	if(isSend === false){
-		isSend = true;
-		resetFormError(message);
-		return;
-	}
-	if(newQuery){
-		newQuery = false;
-    	$.ajax({
-			url     : 'php/notify-me.php',
-			type    : 'POST',
-			data    : form.serialize(),
-			success : function(data){
-				form.find('.btn').prop('disabled', true);
-				message.removeClass('text-danger').removeClass('text-success').fadeIn();
-				switch(data) {
-					case 0:
-						message.html(messageSuccess).addClass('text-success').fadeIn();
-						setTimeout(function(){
-							message.removeClass('text-success').fadeOut(10);
-							newQuery = true;
-						}, 3000);
-						setTimeout(function(){
-							form.trigger('reset');
-							message.fadeOut().delay(500).queue(function(){
-								message.html('').dequeue();
-								newQuery = true;
-							});
-						}, 2000);
-						break;
-					case 1:
-						message.html(messageInvalid).addClass('text-danger').fadeIn();
-						 $('.form-control').on('keydown',resetForm);
-						 // $('.form-control').css('color',"#fd6967");
-						 isSend = false;
-						break;
-					case 2:
-						message.html(messageSigned).addClass('text-danger').fadeIn();
-						setTimeout(function(){
-							form.trigger('reset');
-							message.queue(function(){
-								message.html('').dequeue();
-							});
-							newQuery = true;
-						}, 2000);
-						break;
-					default:
-						message.html(messageErrore).addClass('text-danger').fadeIn();
-				}
-				form.find('.btn').prop('disabled', false);
-			}
-		});
-	}
-});
+// $('.notify-me').submit(function(e){
+// 	var form           = $(this),
+// 		message        = form.find('.form-message'),
+// 		messageSuccess = 'Your email has been sent',
+// 		messageInvalid = 'Please enter a valid email address',
+// 		messageSigned  = 'This email is already signed',
+// 		messageErrore  = 'Error request';
+// 	e.preventDefault();
+// 	if(isSend === false){
+// 		isSend = true;
+// 		resetFormError(message);
+// 		return;
+// 	}
+// 	if(newQuery){
+// 		newQuery = false;
+//     	$.ajax({
+// 			url     : 'php/notify-me.php',
+// 			type    : 'POST',
+// 			data    : form.serialize(),
+// 			success : function(data){
+// 				form.find('.btn').prop('disabled', true);
+// 				message.removeClass('text-danger').removeClass('text-success').fadeIn();
+// 				switch(data) {
+// 					case 0:
+// 						message.html(messageSuccess).addClass('text-success').fadeIn();
+// 						setTimeout(function(){
+// 							message.removeClass('text-success').fadeOut(10);
+// 							newQuery = true;
+// 						}, 3000);
+// 						setTimeout(function(){
+// 							form.trigger('reset');
+// 							message.fadeOut().delay(500).queue(function(){
+// 								message.html('').dequeue();
+// 								newQuery = true;
+// 							});
+// 						}, 2000);
+// 						break;
+// 					case 1:
+// 						message.html(messageInvalid).addClass('text-danger').fadeIn();
+// 						 $('.form-control').on('keydown',resetForm);
+// 						 // $('.form-control').css('color',"#fd6967");
+// 						 isSend = false;
+// 						break;
+// 					case 2:
+// 						message.html(messageSigned).addClass('text-danger').fadeIn();
+// 						setTimeout(function(){
+// 							form.trigger('reset');
+// 							message.queue(function(){
+// 								message.html('').dequeue();
+// 							});
+// 							newQuery = true;
+// 						}, 2000);
+// 						break;
+// 					default:
+// 						message.html(messageErrore).addClass('text-danger').fadeIn();
+// 				}
+// 				form.find('.btn').prop('disabled', false);
+// 			}
+// 		});
+// 	}
+// });
